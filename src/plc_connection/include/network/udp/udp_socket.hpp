99b844b276dac4b9134046e8dbd5047d0490cc13
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-05-05 20:39:47 +0200

    Update udp_socket.hpp

--- src/plc_connection/include/network/udp/udp_socket.hpp
+++ src/plc_connection/include/network/udp/udp_socket.hpp
@@ -1,33 +1,25 @@
-#ifndef UDP_SOCKET_H
-#define UDP_SOCkET_H
+#ifndef OWN_UDP_SOCKET_HPP
+#define OWN_UDP_SOCKET_HPP
 
 #include "network/udp/udp.hpp"
 
 namespace OwnUDP
 {
-    class UDPSocket:private OwnSocket::Socket
+    class UDPSocket : private OwnSocket::Socket
     {
-        public:
-        //Constructors and Destructors
+    public:
         UDPSocket();
         UDPSocket(Address* OwnIP);
         ~UDPSocket();
-        
-        //Set/Get Address
+
         void bindAddress(Address* OwnIP);
         void getAddress(Address* IP);
 
-        //Set Timeout
         void setReceiveTime(int usec, int sec);
 
-        //Write and read data
         void write(uint8_t* Data, int length, Address* IP);
         void read(uint8_t* Data, int length, Address* IP);
-
-        private:
-
     };
-
 }
 
 #endif
