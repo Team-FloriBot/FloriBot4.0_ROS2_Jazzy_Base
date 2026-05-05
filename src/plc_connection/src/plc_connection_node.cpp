0b2df186fde6339ee7214f8e3ed23ac7ffaa6270
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-05-05 20:36:53 +0200

    Update plc_connection_node.cpp

--- src/plc_connection/src/plc_connection_node.cpp
+++ src/plc_connection/src/plc_connection_node.cpp
@@ -2,7 +2,32 @@
 
 void ExitFcn();
 
+int main(int argc, char** argv)#include "plc_connection/plc_connection.hpp"
+
 int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
+
+    auto node = std::make_shared<PlcConnectionNode>();
+
+    try
+    {
+        rclcpp::spin(node);
+    }
+    catch (const std::exception& e)
+    {
+        RCLCPP_ERROR(
+            rclcpp::get_logger(node->get_name()),
+            "PLC connection node exited with exception: %s",
+            e.what());
+
+        rclcpp::shutdown();
+        return 1;
+    }
+
+    rclcpp::shutdown();
+    return 0;
+}
 {
     // Initialisiere ROS2
     rclcpp::init(argc, argv); 
