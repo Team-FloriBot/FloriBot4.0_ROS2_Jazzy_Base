f4de6ceeb5b162fd987d69074497f93af63ae47e
norikaschneider <norikaschneider@gmail.com>
2026-05-13 14:34:12 +0200

    Refactor launch file to include simulation time argument and update URDF parameters

--- src/robot_description/src/launch/launch.py
+++ src/robot_description/src/launch/launch.py
@@ -1,12 +1,16 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 import os
 import xacro
 
 def generate_launch_description():
-
     package_name = "robot_description"
+    
+    # Sim-Time Argument (Standard auf True, falls du Gazebo nutzt)
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
     urdf_path = os.path.join(
         get_package_share_directory(package_name),
@@ -14,20 +18,32 @@ def generate_launch_description():
         "Floribot.urdf.xacro"
     )
     
-    # Process xacro file
     doc = xacro.process_file(urdf_path)
     robot_description_content = doc.toxml()
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
+
         Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
+            name="robot_state_publisher",
             parameters=[{
-                "robot_description": robot_description_content
+                "robot_description": robot_description_content,
+                "use_sim_time": use_sim_time
             }]
         ),
+        
         Node(
             package="joint_state_publisher",
-            executable="joint_state_publisher"
+            executable="joint_state_publisher",
+            name="joint_state_publisher",
+            parameters=[{
+                "robot_description": robot_description_content, # Unverzichtbar!
+                "use_sim_time": use_sim_time
+            }]
         )
     ])
