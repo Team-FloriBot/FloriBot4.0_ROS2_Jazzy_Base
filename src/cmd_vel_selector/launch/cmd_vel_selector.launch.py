4d990a7756ac52e303c09c458459521bb9e539b4
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-05-26 14:56:39 +0200

    Update cmd_vel_selector.launch.py

--- src/cmd_vel_selector/launch/cmd_vel_selector.launch.py
+++ src/cmd_vel_selector/launch/cmd_vel_selector.launch.py
@@ -1,1 +1,28 @@
+import os
 
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    package_share_directory = get_package_share_directory('cmd_vel_selector')
+
+    parameter_file = os.path.join(
+        package_share_directory,
+        'config',
+        'cmd_vel_selector.yaml'
+    )
+
+    cmd_vel_selector_node = Node(
+        package='cmd_vel_selector',
+        executable='cmd_vel_selector_node',
+        name='cmd_vel_selector',
+        output='screen',
+        parameters=[parameter_file]
+    )
+
+    return LaunchDescription([
+        cmd_vel_selector_node
+    ])
